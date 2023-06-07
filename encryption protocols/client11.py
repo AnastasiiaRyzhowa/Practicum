24620ebd11094da05b91a55c96f2d4368d29b48b
AnastasiiaRyzhowa <89018906+AnastasiiaRyzhowa@users.noreply.github.com>
2023-06-07 14:03:16 +0300

    Update client11.py

--- encryption protocols/client11.py
+++ encryption protocols/client11.py
@@ -1,27 +1,19 @@
 import socket
 import pickle
-import os
-
+import cryptocode
 HOST = '127.0.0.1'
-PORT = 9090
-
+PORT = 8080
 sock = socket.socket()
 sock.connect((HOST, PORT))
-p, g, a = 7, 5, 3
+p, g, a = 56, 23, 9
 A = g ** a % p
-sock.send(pickle.dumps((p, g, A))) #отправка чисел
-
-
+sock.send(pickle.dumps((p, g, A)))  #отправка чисел серверу
 B = pickle.loads(sock.recv(1024))  # полученные данные
 K = B ** a % p
 key = str(K)
-
-cryptmess = pickle.loads(sock.recv(1024))
-print(cryptmess)
-
-decrmess = cryptocode.decrypt(cryptmess, key)
-print(decrmess)
+cipherword = pickle.loads(sock.recv(1024))
+print('Шифруем сообщение:', cipherword)
+word = cryptocode.decrypt(cipherword, key)
+print('Расшифровываем сообщение:', word)
 
 sock.close()
-
-
